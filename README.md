0674c372fa8734b01390d49eb5ca1caccd39d935
rene_mbp <harryloveslearning@googlemail.com>
2016-11-14 22:08:29 +0100

    draw & drop of the lines

--- README.md
+++ README.md
@@ -4,6 +4,7 @@ Working with Angular2 and SVG
 
 Look to that [useful blog](http://blog.500tech.com/svg-in-angular-2/)
 
+Thanx to [Pascal Precht](https://github.com/PascalPrecht/ng-be-angular2-demos)
 
 use:
    
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,9 +7,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
 
-  elements = [
-    { type:"line", x1:30, y1:140, x2:20, y2:40}
-  ]
+  currentId = null;
+  startX;
+  startY;
+  elements = []
+
+  ngOnInit() {
+    this.appendElement(
+      { type:"line", x1:30, y1:140, x2:20, y2:40}
+    );
+  }
 
   randInt(max) {
     return Math.floor( Math.random() * max);
@@ -17,6 +24,7 @@ export class AppComponent {
 
   newElement() {
     return {
+      id: null,
       type:"line",
       x1: this.randInt(400),
       y1: this.randInt(400),
@@ -25,7 +33,51 @@ export class AppComponent {
     }
   }
 
-  onAdd() {
-    this.elements.push( this.newElement() )
+  appendElement(ele) {
+    ele.id = this.elements.length;
+    this.elements.push(ele);
   }
+
+
+  onAdd() {
+    let ele = this.newElement();
+    this.appendElement(ele);
+  }
+
+  mouseMove(event) {
+    if (this.currentId != null) {
+      this.updateLine(this.currentId, event.clientX, event.clientY);
+    }
+  }
+
+  mouseUp(event) {
+    this.currentId = null;
+  }
+
+  mouseDown(event) {
+    const id = Number(event.target.getAttribute("id"));
+    const line = this.elements[id];
+    this.startX = event.clientX;
+    this.startY = event.clientY;
+    this.currentId = id;
+  }
+
+  updateLine(id, newX, newY) {
+    let dx = newX - this.startX;
+    let dy = newY - this.startY;
+    console.log(dx + "/" + dy);
+    this.startX += dx;
+    this.startY += dy;
+
+    var ele = this.elements[id]
+    ele.x1 += dx;
+    ele.y1 += dy;
+    ele.x2 += dx;
+    ele.y2 += dy;
+
+    this.elements[id] = ele;
+
+  }
+
+
 }
