ed72e56de5c2fb2b4d064200dc6d3a09f3ba5073
rene_mbp <harryloveslearning@googlemail.com>
2016-11-23 22:30:49 +0100

    line & zoom

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,29 +7,57 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
 
+  canvas = {
+    width: 600,
+    height: 600
+  }
+
+  viewport = {
+    x1: 0,
+    y1: 0,
+    x2: 600,
+    y2: 600
+
+  }
+
   currentId = null;
   startX;
   startY;
   elements = []
 
   ngOnInit() {
-    this.appendElement(
-      { type:"line", x1:30, y1:140, x2:20, y2:40}
-    );
+    this.appendElement( { type:"line", x1:30, y1:140, x2:20, y2:40} );
+    this.appendElement( { type:"text", x:130, y:140, text:"hallo"} );
   }
+
+  getViewport() {
+    let vp = `${this.viewport.x1} ${this.viewport.y1} ${this.viewport.x2} ${this.viewport.y2}`
+    return vp
+  }
+
 
   randInt(max) {
     return Math.floor( Math.random() * max);
   }
 
-  newElement() {
+  newLine() {
     return {
       id: null,
       type:"line",
-      x1: this.randInt(400),
-      y1: this.randInt(400),
-      x2: this.randInt(400),
-      y2: this.randInt(400)
+      x1: this.randInt(this.canvas.width),
+      y1: this.randInt(this.canvas.height),
+      x2: this.randInt(this.canvas.width),
+      y2: this.randInt(this.canvas.height)
+    }
+  }
+
+  newText() {
+    return {
+      id: null,
+      type:"text", 
+      x: this.randInt(this.canvas.width), 
+      y: this.randInt(this.canvas.height),
+      text:"Text-" + this.randInt(100) 
     }
   }
 
@@ -39,9 +67,40 @@ export class AppComponent {
   }
 
 
-  onAdd() {
-    let ele = this.newElement();
+  addLine() {
+    let ele = this.newLine();
     this.appendElement(ele);
+  }
+
+  addText() {
+    let ele = this.newText();
+    this.appendElement(ele);
+    
+  }
+
+  zoomIn() {
+    let dx = this.viewport.x2 - this.viewport.x1
+    let dy = this.viewport.y2 - this.viewport.y1
+
+    if (dx > 10 &&  dx > 10) {
+      this.viewport.x1 += 0.1 * dx
+      this.viewport.x2 -= 0.1 * dx
+      this.viewport.y1 += 0.1 * dy
+      this.viewport.y2 -= 0.1 * dy
+    }
+  }
+
+  zoomOut() {
+    let dx = this.viewport.x2 - this.viewport.x1
+    let dy = this.viewport.y2 - this.viewport.y1
+
+    if (dx < 1000 &&  dx < 1000) {
+      this.viewport.x1 -= 0.1 * dx
+      this.viewport.x2 += 0.1 * dx
+      this.viewport.y1 -= 0.1 * dy
+      this.viewport.y2 += 0.1 * dy
+    }
+
   }
 
   mouseMove(event) {
@@ -65,7 +124,6 @@ export class AppComponent {
   updateLine(id, newX, newY) {
     let dx = newX - this.startX;
     let dy = newY - this.startY;
-    console.log(dx + "/" + dy);
     this.startX += dx;
     this.startY += dy;
 
